e8ad5064dafe32e7dac4f40318a4b9f35b9ba028
SimpPoseidon <120441879+TanishqMehrunkarIIPSDAVV@users.noreply.github.com>
2024-08-23 22:04:48 +0530

    navbar update

--- src/TeacherDashboard/Navbar.jsx
+++ src/TeacherDashboard/Navbar.jsx
@@ -6,21 +6,21 @@ const Navbar=()=>
 {
     return(
         <>
-        <div className="navbar">
-            <div className="contents left-margin">
+        <div className="navbar-tanishq">
+            <div className="contents-tanishq left-margin-tanishq">
                 <div>Niko Vajradanti</div>
                 <img alt="Image" className="pfp"
                 src="https://st4.depositphotos.com/9998432/22597/v/450/depositphotos_225976914-stock-illustration-person-gray-photo-placeholder-man.jpg" 
                 width="50" height="50"/>
             </div>
-            <div className="contents">
-                <button className="papers">Papers</button>
+            <div className="contents-tanishq">
+                <button className="papers-tanishq">Papers</button>
                 <div>
                     Ready for Evaluation
                 </div>
             </div>
-            <div className="right-margin">
-                <button className="logout">Logout</button>
+            <div className="right-margin-tanishq">
+                <button className="logout-tanishq">Logout</button>
             </div>
         </div>
         </>
--- src/TeacherDashboard/TeacherDashboard.jsx
+++ src/TeacherDashboard/TeacherDashboard.jsx
@@ -1,11 +1,10 @@
 import React from 'react';
-import Navbar from "./Navbar";
+// import Navbar from "./Navbar";
 
 const TeacherDashboard=()=>
 {
     return(
         <>
-        <Navbar />
         </>
     );
 }
