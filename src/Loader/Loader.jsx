a1f456d5fbfb85d2b6d767e83db5510a2a3e213c
Nishant Kaushal <101548649+nishant0708@users.noreply.github.com>
2024-10-14 23:39:52 +0530

    updated

--- src/Loader/Loader.jsx
+++ src/Loader/Loader.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import "./Loader.css";
-const Loader=()=>
-{
-    return(
-        <>
-        <div className="loader-div">
-            <div className="loader"></div>
-        </div>
-        </>
-    );
-}
+const Loader = () => {
+  return (
+    <>
+      <div className="loader-div">
+        <div className="loader"></div>
+      </div>
+    </>
+  );
+};
 
 export default Loader;
